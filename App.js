afdf60285aee154a312653367c728f789b54f062
Matheus Ibarra <matheusibarra98@gmail.com>
2021-03-15 09:57:10 -0300

    Primeiro Commit

---
+++ App.js
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import Slider from '@react-native-community/slider'
+import Clipboard from 'expo-clipboard'
+
+let charset = 'abcefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ0123456789!@#$%¨&*()-='
+
+export default function app() {
+    const [password, setPassword] = useState('');
+    const [size, setSize] = useState(5);
+
+    function generatePass(){
+        let pass = '';
+        for (let i = 0, n = charset.length; i < size; i++) {
+            pass += charset.charAt(Math.floor(Math.random() * n))
+        }
+        setPassword(pass);
+    }
+
+    function copyPass(){
+        Clipboard.setString(password);
+        alert ('Senha copiada com sucesso!');
+    }
+
+  return(
+    <View style={styles.container}>
+        <Image
+         source={require('../gerador/assets/logo.png')}
+        style={styles.logo}
+        />
+        <Text style={styles.title}>{size} Caracteres</Text>
+
+        <View style={styles.area}>
+            <Slider
+                style={{height: 50}}
+                minimumValue={5}
+                maximumValue={15}
+                minimumTrackTintColor="#FF0000"
+                maximumTrackTintColor="#000"
+                value={size}
+                onValueChange={(value) => setSize(value.toFixed(0))}
+            />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={generatePass}>
+            <Text style={styles.buttonText}>Gerar senha</Text>
+        </TouchableOpacity>
+        {password !== '' &&
+            <>
+            <View style={styles.area}>
+                <Text onLongPress={copyPass} style={styles.password}>{password}</Text>
+            </View>
+            <TouchableOpacity style={styles.buttonErase} onPress={() => setPassword('')}>
+                <Text style={styles.buttonText}>Retirar senha</Text>
+            </TouchableOpacity>
+            </>
+            
+        }
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f3f3ff'
+  },
+  logo:{
+    marginBottom: 60
+  },
+  title:{
+      fontSize: 30,
+      fontWeight: 'bold'
+  },
+  area:{
+      marginTop: 15,
+      marginBottom: 15,
+      backgroundColor: '#fff',
+      width: '80%',
+      borderRadius: 7
+  },
+  button:{
+      backgroundColor: '#FFa200',
+      width: '80%',
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 7,
+      marginBottom: 25
+  },
+  buttonErase:{
+    backgroundColor: '#FF0000',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25
+},
+  buttonText:{
+      fontSize: 20,
+      color: '#fff',
+      fontWeight: 'bold'
+  },
+  
+  password:{
+      padding: 10,
+      textAlign: 'center',
+      fontSize: 20
+  }
+})
